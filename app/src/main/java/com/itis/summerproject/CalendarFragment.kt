15f94b05808fac430fef4c6b6d30d01609594c54
AnyaMostyakova <anyamostyakova2004@icloud.com>
2023-07-08 13:15:56 +0300

    Anya calendar and settings

--- app/src/main/java/com/itis/summerproject/CalendarFragment.kt
+++ app/src/main/java/com/itis/summerproject/CalendarFragment.kt
@@ -1,6 +1,47 @@
 package com.itis.summerproject
 
 import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.View
+import android.widget.*
 
 class CalendarFragment : Fragment(R.layout.fragment_calendar) {
+    private lateinit var selectedDate: String
+    private val workouts = mutableListOf<Pair<String, String>>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val calendarView = view.findViewById<CalendarView>(R.id.calendarView)
+        val workoutNameEditText = view.findViewById<EditText>(R.id.workoutNameEditText)
+        val addWorkoutButton = view.findViewById<Button>(R.id.addWorkoutButton)
+        val workoutListView = view.findViewById<ListView>(R.id.workoutListView)
+
+        calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            selectedDate = "$dayOfMonth-${month + 1}-$year"
+            updateWorkoutListView()
+        }
+
+        addWorkoutButton.setOnClickListener {
+            val workoutName = workoutNameEditText.text.toString().trim()
+            if (workoutName.isNotEmpty()) {
+                workouts.add(Pair(selectedDate, workoutName))
+                workoutNameEditText.text.clear()
+                updateWorkoutListView()
+            } else {
+                Toast.makeText(requireContext(), "Введите название тренировки", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, workouts.map { "${it.first}: ${it.second}" })
+        workoutListView.adapter = adapter
+    }
+
+    private fun updateWorkoutListView() {
+        val workoutListView = requireView().findViewById<ListView>(R.id.workoutListView)
+        val adapter = workoutListView.adapter as ArrayAdapter<String>
+        adapter.clear()
+        adapter.addAll(workouts.map { "${it.first}: ${it.second}" })
+        adapter.notifyDataSetChanged()
+    }
 }
--- app/src/main/java/com/itis/summerproject/SettingsFragment.kt
+++ app/src/main/java/com/itis/summerproject/SettingsFragment.kt
@@ -1,6 +1,43 @@
 package com.itis.summerproject
 
+import android.content.res.Configuration
+import android.os.Bundle
+import android.text.InputType
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Switch
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 
 class SettingsFragment: Fragment(R.layout.fragment_settings)  {
+    private lateinit var themeToggleBtn: Switch
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+        themeToggleBtn = view.findViewById(R.id.switch1)
+        themeToggleBtn.setOnClickListener {
+            toggleTheme()
+        }
+        return view
+    }
+
+    private fun toggleTheme() {
+        val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        when (currentNightMode) {
+            Configuration.UI_MODE_NIGHT_NO -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+            Configuration.UI_MODE_NIGHT_YES -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+        activity?.recreate()
+    }
+
 }
