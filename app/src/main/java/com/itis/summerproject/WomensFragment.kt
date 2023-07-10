9df609261018f2963c510e47f794886cf737ba1a
AnyaMostyakova <anyamostyakova2004@icloud.com>
2023-07-10 18:45:45 +0300

    anya button назад

--- app/src/main/java/com/itis/summerproject/WomensFragment.kt
+++ app/src/main/java/com/itis/summerproject/WomensFragment.kt
@@ -4,11 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 
 class WomensFragment : Fragment(R.layout.fragment_womens) {
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_womens, container, false)
+        val view = inflater.inflate(R.layout.fragment_womens, container, false)
+
+        val backButton = view.findViewById<Button>(R.id.backButton)
+        backButton.visibility = View.VISIBLE
+        backButton.setOnClickListener {
+            requireActivity().supportFragmentManager.popBackStack()
+        }
+
+        return view
     }
+
+
+
 }
